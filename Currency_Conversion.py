0e13b289b850ec37abdf6a2b2e3008850bcf2d82
Aman Pandit <amanpandit3027@gmail.com>
2023-02-14 22:02:35 +0530

    Create Currency_Conversion.py
    This code is a basic currency conversion tool that allows the user to enter the amount to be converted, its currency code, and the currency code to convert to. The code converts the amount using the forex python module and outputs the converted rate. A try-except block is also included in the code to handle any issues that may arise during the conversion process.

---
+++ Currency_Conversion.py
@@ -0,0 +1,30 @@
+# A Quick Currency Conversion with forex_python.
+
+import forex_python.converter
+# import CurrencyRates
+
+# Create an instance of CurrencyRates class from forex_python.converter
+converter = forex_python.converter.CurrencyRates()
+try:
+
+    # Get the amount to be converted from the user
+    amount = int(input('Enter the desired amount you want to convert:\n'))
+
+    # Get the currency code that has to be converted from the user
+    from_currency = input(
+        'Enter the currency code that has to be converted:\n').upper()
+
+    # Get the currency code to convert to from the user
+    to_currency = input('Enter the currency code to convert:\n').upper()
+
+    # Print the conversion details
+    print('You are converting', amount, from_currency, 'to', to_currency, '.')
+
+    # Perform the conversion and store the result in variable output
+    output = converter.convert(from_currency, to_currency, amount)
+
+    # Print the converted rate
+    print('The converted rate is', output)
+
+except Exception as e:
+    print("An error occurred: ", str(e))
